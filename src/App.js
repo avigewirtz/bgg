dad521f4f3e8cbe617ac4fd9eb71be2792a18257
Avi Gewirtz <sgewirtz@princeton.edu>
2023-09-01 13:34:05 -0400

    netlify2

--- src/App.js
+++ src/App.js
@@ -273,24 +273,24 @@ Peretz Bronstein or Yael Nathanson
     );
 };
 
-function generateDocument(content) {
-    const doc = new Document({
-        sections: [
-            {
-                children: [
-                    new Paragraph({
-                        children: [
-                            new TextRun(content),
-                        ],
-                    }),
-                ],
-            },
-        ],
-    });
-
-    // Packer to DOCX
-    return Packer.toBlob(doc);
-}
+//function generateDocument(content) {
+//    const doc = new Document({
+//        sections: [
+//            {
+//                children: [
+//                    new Paragraph({
+//                        children: [
+//                            new TextRun(content),
+//                        ],
+//                    }),
+//                ],
+//            },
+//        ],
+//    });
+//
+//    // Packer to DOCX
+//    return Packer.toBlob(doc);
+//}
 
 const downloadDocument = (content) => {
     const paragraphs = contentToParagraphs(content);
