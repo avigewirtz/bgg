cb38ede4f7a70917128eaa6660cc9dfb94d9e867
Avi Gewirtz <avigewirtz@avis-mbp.mynetworksettings.com>
2024-06-10 12:40:31 -0400

    contact

--- src/mainContent.js
+++ src/mainContent.js
@@ -361,7 +361,8 @@ const createNewWordpressPage = async () => {
 
     const jotFormScriptUrl = `https://form.jotform.com/jsform/233467061911151?caseType=${encodedFullName}`;
     const htmlBlock = `<style>#footer .contact-form {display: none !important;}</style><a id="sign-up"></a><script type="text/javascript" src="${jotFormScriptUrl}"></script>`;  
-    const fullContent = generatedContentSite + htmlBlock;
+    const shortcode = '[contact_info]';
+    const fullContent = generatedContentSite + shortcode + htmlBlock;
   
     const headers = {
         'Content-Type': 'application/json',
