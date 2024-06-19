bcdf7f6cd49d0b2e53aec203e0fd20655db1b42d
Avi Gewirtz <sgewirtz@princeton.edu>
2024-06-19 11:08:36 -0400

    prisma

---
+++ prisma/migrations/20240619150820_new_case_type/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "PressRelease" ADD COLUMN     "otherExchange" STRING;
+ALTER TABLE "PressRelease" ADD COLUMN     "otherFullName" STRING;
+ALTER TABLE "PressRelease" ADD COLUMN     "otherShortName" STRING;
+ALTER TABLE "PressRelease" ADD COLUMN     "otherTicker" STRING;
